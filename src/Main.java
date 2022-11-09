1e32da7bb4093559cbda7ef32ffdde48ff6579be
Антон <antonolegovich2017@icloud.com>
2022-11-09 16:27:21 +0600

    dfdfg

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        MAP_COLLECTION.map.put("str1",4);
+        MAP_COLLECTION.map.put("str2",3);
+        MAP_COLLECTION.map.put("str3",8);
+        MAP_COLLECTION.map.put("str4",1);
+        MAP_COLLECTION.map.put("str5",9);
+        MAP_COLLECTION.map.put("str6",2);
+
+
+    }
+}
