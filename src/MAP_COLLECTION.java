c67afcd8b5a4a79f463872e7c316431baed61a62
Антон <antonolegovich2017@icloud.com>
2022-11-09 16:29:38 +0600

    dfdfg

--- src/MAP_COLLECTION.java
+++ src/MAP_COLLECTION.java
@@ -4,15 +4,14 @@ import java.util.Map;
 public class MAP_COLLECTION {
 
 
-   public static Map<String, Integer> map = new HashMap<>();
+    public static Map<String, Integer> map = new HashMap<>();
 
 
-   public static void addToMap(String str,Integer count){
-      if (map.containsKey(str)&&map.get(str).equals(count)){
-         throw new RuntimeException(" Такое значение уже есть");
-      } else{
-         map.put(str,count);
-         
-      }
-   }
+    public static void addToMap(String str, Integer count) {
+        if (map.containsKey(str) && map.get(str).equals(count)) {
+            throw new RuntimeException(" Такое значение уже есть");
+        }
+        map.put(str, count);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
         MAP_COLLECTION.map.put("str4",1);
         MAP_COLLECTION.map.put("str5",9);
         MAP_COLLECTION.map.put("str6",2);
-
+        System.out.println(MAP_COLLECTION.map);
+        MAP_COLLECTION.addToMap("str5",4);
+        System.out.println(MAP_COLLECTION.map);
 
     }
 }
